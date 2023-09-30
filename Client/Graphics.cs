d1f9efead87f7019773c8b3c8a6f40486e00efc4
Makar Cherezov <cherezov.m003@gmail.com>
2023-09-30 16:55:12 +0700

    Разделение проекта прошлой лабораторной на клиент и сервер.

---
+++ Client/Graphics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Client
+{
+    public static class Graphics
+    {
+        public static string Logo { get; set; }
+        public static string Exit { get; set; }
+        public static string Line { get; set; }
+        
+        public static string GetLogo()
+        {
+            Logo = File.ReadAllText(@"C:\Учёба\Архитектура ИС\ЛР 1\ЛР 1 консоль\Logo.txt");
+            return Logo;
+        }
+
+        public static string GetExit()
+        {
+            Exit = File.ReadAllText(@"C:\Учёба\Архитектура ИС\ЛР 1\ЛР 1 консоль\ExitText.txt");
+            return Exit;
+        }
+        public static string GetLine()
+        {
+            Line = File.ReadAllText(@"C:\Учёба\Архитектура ИС\ЛР 1\ЛР 1 консоль\Line.txt");
+            return Line;
+        }
+            
+    }
+    
+}
---
+++ ЛР 1 консоль/ProductData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class ProductData : IDataStructure
+    {
+        public string ProductName { get; set; }
+        public string SellerName { get; set; }
+        public string ProductDescription { get; set; }
+        public float Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime DateOfUpdating { get; set; }
+
+        public ProductData(string productName, string sellerName, string productDescription, float price, bool isAvailable, DateTime date)
+        {
+            ProductName = productName;
+            SellerName = sellerName;
+            ProductDescription = productDescription;
+            Price = price;
+            IsAvailable = isAvailable;
+            DateOfUpdating = date;
+        }
+        public List<string> GetPrintableStrings()
+        {
+            List<string> fields = new List<string>();
+            fields.Add(ProductName);
+            fields.Add(SellerName);
+            fields.Add(ProductDescription);
+            fields.Add(Price.ToString());
+            fields.Add(IsAvailable.ToString());
+            fields.Add(DateOfUpdating.ToString());
+            return fields;
+        }
+    }
+}
